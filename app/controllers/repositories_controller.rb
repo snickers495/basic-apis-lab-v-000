03cc6bd319b909a661fd4dd697f5a3b8c0579ae8
Lisa Lee <snickers495@gmail.com>
2018-03-08 17:33:28 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,14 +3,15 @@ class RepositoriesController < ApplicationController
   def search
 
   end
-  
-  resp = Faraday.get("https://api.github.com/search/repositories") do |req|
-     req.params['client_id'] = ''
-     req.params['client_secret'] = ''
-      req.params['q'] = params[:query]
 
-    end
-    body = JSON.parse(resp.body)
-    @results = body["items"]
-    render 'search'
+  def github_search
+    resp = Faraday.get("https://api.github.com/search/repositories") do |req|
+       req.params['client_id'] = ''
+       req.params['client_secret'] = ''
+        req.params['q'] = params[:query]
+
+      end
+      body = JSON.parse(resp.body)
+      @results = body["items"]
+      render 'search'
 end
